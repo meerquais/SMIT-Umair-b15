41766ffafa0832fa60d3c53bc6f26aae0ab5cf40
Meer Quais <meerquais017@gmail.com>
2025-04-18 20:05:22 +0500

    JavaScript Revision

---
+++ Class 1/app.js
@@ -0,0 +1,15 @@
+console.log("Js Connected.")
+
+
+
+
+// function container(){
+//     document.getElementById("myDiv").style.backgroundColor = "red" ; 
+// }
+
+
+var button = document.getElementById("myBtn");
+
+button.addEventListener("click" , function(){
+    console.log("Button was Clicked!")
+})
