88933c2f02739491fe293a36069e3ae7a2c86d4f
Meer Quais <meerquais017@gmail.com>
2025-05-07 22:57:47 +0500

    Fixed the math expressions

--- Class 07/app.js
+++ Class 07/app.js
@@ -108,9 +108,9 @@ const timer = setInterval(()=>{
     // time breakdown : days , hours , minutes and seconds
 
     const days = Math.floor(difference / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((difference % (1000 * 60 * 60 *24))/ (1000 * 60 * 60));
-    const minutes = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60));
-    const seconds = Math.floor((difference % (1000 * 60 * 60 * 24)) / 1000 );
+    const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((difference % (1000 * 60)) / 1000);
 
     // result show karna hai.
 
